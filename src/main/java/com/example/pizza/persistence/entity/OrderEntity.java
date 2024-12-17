e12baefa4bb3868197f2f8d722cbd8514ac2ff03
steeltx <realsteel.gnu@gmail.com>
2024-12-17 00:15:49 -0600

    Configuración de BD, entidades de pizza y order

---
+++ src/main/java/com/example/pizza/persistence/entity/OrderEntity.java
@@ -0,0 +1,31 @@
+package com.example.pizza.persistence.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "pizza_order")
+public class OrderEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_order", nullable = false)
+    private Integer idOrder;
+
+    @Column(name = "id_customer", nullable = false, length = 15)
+    private String idCustomer;
+
+    @Column(nullable = false, columnDefinition = "DATETIME")
+    private LocalDateTime date;
+
+    @Column(nullable = false, columnDefinition = "DECIMAL(6,2)")
+    private Double price;
+
+    @Column(nullable = false, columnDefinition = "CHAR(1)")
+    private String method;
+
+    @Column(name = "additional_notes", length = 200)
+    private String additionalNotes;
+
+}
