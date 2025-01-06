a0c1dbbc83736628b65ed881223c6b5ebcaba9fa
steeltx <realsteel.gnu@gmail.com>
2025-01-05 23:25:39 -0600

    Uso de query methods en pizza

--- src/main/java/com/example/pizza/persistence/repository/PizzaRepository.java
+++ src/main/java/com/example/pizza/persistence/repository/PizzaRepository.java
@@ -3,6 +3,12 @@ package com.example.pizza.persistence.repository;
 import com.example.pizza.persistence.entity.PizzaEntity;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.List;
+
 public interface PizzaRepository extends ListCrudRepository<PizzaEntity, Integer> {
+
+    List<PizzaEntity> findAllByAvailableTrueOrderByPrice();
+
+    PizzaEntity findAllByAvailableTrueAndNameIgnoreCase(String name);
 
 }
